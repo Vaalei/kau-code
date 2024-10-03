732df4accaa5b7638bca8af65f27c090df9113da
Vaalei <ville@tormanen.nu>
2024-10-03 13:46:26 +0200

    Add polishing touches

--- dvgb19/labb1.c
+++ dvgb19/labb1.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <signal.h>
 
 #define FIFO_NAME "file.fifo"
 #define MAX_LENGTH 1000
@@ -18,7 +19,6 @@ int TimeInterval;
 sem_t buffer;
 sem_t zero;
 
-
 void *producer(){
     char str[MAX_LENGTH];
 	int fd;
@@ -28,12 +28,12 @@ void *producer(){
     printf("producer is created\n");
     while(1)
     {
-        sem_wait(&buffer);
-        pthread_mutex_lock(&lock);
-        write(fd, str, strlen(str));
-        pthread_mutex_unlock(&lock);
+        sem_wait(&buffer); /*waits on the buffer semaphore*/
+        pthread_mutex_lock(&lock);  /*lock before entering critical section */
+        write(fd, str, strlen(str)); /*Critical section (writing to the FIFO)*/
+        pthread_mutex_unlock(&lock); /*Unlock after exiting the critical function*/
         printf("producer wrote %s", str);
-        sem_post(&zero);
+        sem_post(&zero); /*Signals the consumer that an item is available */
         sleep(TimeInterval);
     }
     close(fd);
@@ -42,22 +42,31 @@ void *producer(){
 void* consumer(void *thread_id){
     char str[MAX_LENGTH];
 	int fd;
-    long tid = (long)thread_id;
+    long tid = (long)pthread_self();
     fd = open(FIFO_NAME, O_RDONLY); /* open FIFO for reading */
     printf("consumer #%ld is created\n", tid);
     while (1)
     {
         sem_wait(&zero);
-        pthread_mutex_lock(&lock);
-        read(fd, str, MAX_LENGTH);
-        sem_post(&buffer);
-        pthread_mutex_unlock(&lock);
+        pthread_mutex_lock(&lock); /*lock before entering the critical section*/
+        read(fd, str, MAX_LENGTH); /*Critical section (Reading from the FIFO)*/
+        sem_post(&buffer); 
+        pthread_mutex_unlock(&lock); /*Unlock after exiting the critical section*/
         printf("Consumer #%ld consumed %s", tid, str);
     }
     close(fd);
 }
 
+void sigint_exit(){
+    pthread_mutex_destroy(&lock);
+    sem_destroy(&buffer);
+    sem_destroy(&zero);
+    printf("exited from function\n");
+    exit(0);
+}
+
 int main(){
+    signal(SIGINT, sigint_exit);
     pthread_t producerid;
     mkfifo(FIFO_NAME, 0666); /* make FIFO, if not already existed*/
     if (pthread_mutex_init(&lock, NULL) != 0)
@@ -75,17 +84,12 @@ int main(){
     pthread_mutex_init(&lock, NULL);
     
     for (int i = 0; i < n; i++) {
-        pthread_create(&tid[i], NULL, consumer, (void *)i);
+        pthread_create(&tid[i], NULL, consumer, NULL);
     }
     pthread_create(&producerid, NULL, producer, NULL);
-
 
     for (int i = 0; i < n; i++) {
         pthread_join(&tid[i], NULL);
     }
     pthread_join(&producerid, NULL);
-
-
-    pthread_mutex_destroy(&lock);
-    sem_destroy(&buffer);
 }
