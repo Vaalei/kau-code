a4af4752cb70c9a84ce843392479a96b5c986872
Vaalei <ville@tormanen.nu>
2024-11-25 14:53:20 +0100

    Add switch case framework

--- dvgb03/labb1/analyze.c
+++ dvgb03/labb1/analyze.c
@@ -1,13 +1,96 @@
 #include "analyze.h"
 #include "algorithm.h"
+#include <time.h>
+#include <stdlib.h>
 
 //
 // Private
 //
+
+// Fill array with random numbers for an apporcimate average
+void fill_random(int *a, int n)
+{
+    srand(time(NULL));
+    for (int i; i < n; i ++)
+    {
+        int r = rand();
+        a[i] = r;
+    }
+}
+
+// Fill array with sorted values in ascending order
+void fill_sorted_asc(int *a, int n)
+{
+    for (int i; i < n; i++)
+    {
+        a[i] = i;
+    }
+}
+// Fill array with sorted values in descending order
+void fill_sorted_desc(int *a, int n)
+{
+    for (int i; i < n; i++)
+    {
+        a[i] = n-i;
+    }
+}
+// Fill array with best case scenario for quicksort
+void fill_quicksort_best_case(int *a, int n)
+{
+
+
+}
+
 
 //
 // Public
 //
 void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
 {
+    int *array = malloc(n*sizeof(int));
+    switch (c)
+    {
+        case best_t:
+            switch (a)
+            {
+                case quick_sort_t:
+                    // TODO
+                    break;
+                
+                default:
+                    fill_sorted_asc(array, n);
+                    break;
+            }
+            break;
+
+        case worst_t:
+            fill_sorted_desc(array, n);
+
+        case average_t:
+            fill_random(array, n);
+            break;
+
+        default:
+            break;
+    }
+    switch (a)
+    {
+        case bubble_sort_t:
+
+            break;
+        case insertion_sort_t:
+
+            break;
+        case quick_sort_t:
+
+            break;
+        case linear_search_t:
+
+            break;
+        case binary_search_t:
+
+            break;
+        default:
+            break;
+    }
 }
--- dvgb03/labb1/ui.c
+++ dvgb03/labb1/ui.c
@@ -49,7 +49,20 @@ static void ui_menu()
 		"Menu",
 		"Exit\n",
 		"Bubble sort best case",
-		// TODO: complete menu
+		"Bubble sort worst case",
+		"Bubble sort average case\n",
+		"Insertion sort best case",
+		"Insertion sort worst case",
+		"Insertion sort average case\n",
+		"Quick sort best case",
+		"Quick sort worst case",
+		"Quick sort average case\n",
+		"Linear search best case",
+		"Linear search worst case",
+		"Linear search average case\n",
+		"Binary search best case",
+		"Binary search worst case",
+		"Binary search average case"
 	};
 
 	ui_line('=', MENU_WIDTH);
@@ -77,14 +90,74 @@ void ui_run()
 			case 'a':
 				show_menu = true;
 				break;
+
+			// Exit
 			case 'b':
 				running = false;
 				break;
-			// Bubble sort
+
+			// Bubble sort best case
 			case 'c':
 				benchmark(bubble_sort_t, best_t, result, RESULT_ROWS);
 				printf("todo> implemenet BE + present results in FE\n");
 				break;
+
+			// Bubble sort worst case
+			case 'd':
+				break;
+				
+			// Bubble sort average case\n
+			case 'e':
+				break;
+
+			// Insertion sort best case
+			case 'f':
+				break;
+
+			// Insertion sort worst case
+			case 'g':
+				break;
+
+			// Insertion sort average case\n
+			case 'h':
+				break;
+
+			// Quick sort best case
+			case 'i':
+				break;
+
+			// Quick sort worst case
+			case 'j':
+				break;
+
+			// Quick sort average case\n
+			case 'k':
+				break;
+
+			// Linear search best case
+			case 'l':
+				break;
+
+			// Linear search worst case
+			case 'm':
+				break;
+
+			// Linear search average case\n
+			case 'n':
+				break;
+
+			// Binary search best case
+			case 'o':
+				break;
+
+			// Binary search worst case
+			case 'p':
+				break;
+
+			// Binary search average cas
+			case 'q':
+				break;
+
 			// Invalid input
 			default:
 				show_menu = false;
