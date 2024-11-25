563f68a987ddfcce985aa9843bf4acaf60c76e02
Vaalei <ville@tormanen.nu>
2024-11-25 14:52:32 +0100

    Add quicksort

--- dvgb03/labb1/algorithm.c
+++ dvgb03/labb1/algorithm.c
@@ -8,7 +8,7 @@
 void swap(int *p1, int *p2)
 {
 	int t;
-	t = p1;
+	t = *p1;
 	*p1 = *p2;
 	*p2 = t;
 }
@@ -29,7 +29,22 @@ void insertion_sort(int *a, int n)
 
 void quick_sort(int *a, int n)
 {
-	// TODO: quick sort
+	int pivot = a[n-1];
+	int pivot_offset = 0;
+	for (int i; i > pivot; i++)
+	{
+		if (a[i] < pivot)
+		{
+			swap(&a[pivot_offset], &a[i]);
+			pivot_offset++;
+		}
+	}
+
+	swap(&a[pivot_offset], &a[n-1]);
+	pivot_offset++;	// Pivot offset + 1 -> Storleken av Arrayen innan pivot 
+
+	quick_sort(a, pivot_offset);
+	quick_sort(&a[pivot_offset], n - pivot_offset);
 }
 
 bool linear_search(const int *a, int n, int v)
