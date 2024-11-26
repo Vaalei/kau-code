9a7d9b2335acc5f9266267a63b7b80cfd1d12b09
Jaber-21 <jaber.mohammed02@outlook.com>
2024-11-26 23:17:42 +0100

    Lade till bubble sort, insertion sort, binary search, liten ändring i makefile.

--- dvgb03/labb1/algorithm.c
+++ dvgb03/labb1/algorithm.c
@@ -20,13 +20,38 @@ void swap(int *p1, int *p2)
 
 void bubble_sort(int *a, int n)
 {
-	// TODO: bubble sort
+	bool swaps;
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 0; j< n-i-1; j++)
+		{
+			if ( a[j] > a[j+1]) {
+				swap(&a[j], &a[j+1]);
+				swaps = true;
+			}
+		}
+		
+	}
+	
 }
 
 void insertion_sort(int *a, int n)
 {
-	// TODO: insertion sort
+	for(int i = 1;i<n;i++) 
+    {
+        int key = a[i];
+        int j = i-1;
+
+        while(j>=0 && key < a[j])
+        {
+           a[j+1] = a[j]; 
+           j-=1;
+        }
+        a[j+1] = key;
+    }
 }
+
+
 
 void quick_sort(int *a, int n)
 {
@@ -67,5 +92,20 @@ bool linear_search(const int *a, int n, int v)
 
 bool binary_search(const int *a, int n, int v)
 {
+	if (n <= 0)
 	return false; // TODO: binary search
+
+	int middle = n/2;
+	int number = a[middle];
+
+	if (number == v)
+	return true;
+
+	else if (number > v)
+	return binary_search (a, middle, v);
+
+	else
+
+	return binary_search (a + middle + 1, n-middle-1, v);
+
 }
