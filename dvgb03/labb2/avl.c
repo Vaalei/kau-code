2f8c11dee14abdf1b1399b43bba34b281fd488e8
Vaalei <ville@tormanen.nu>
2024-12-14 16:48:19 +0100

    Fix Squash bugs in avl

--- dvgb03/labb2/avl.c
+++ dvgb03/labb2/avl.c
@@ -2,7 +2,7 @@
 // avl.c - AVL-tree based on binary search tree (bst.h/bst.c)
 //=============================================================================
 #include "avl.h"
-#define DEBUG 1
+#define DEBUG 0
 //=============================================================================
 // local prototypes
 //-----------------------------------------------------------------------------
@@ -45,30 +45,27 @@ AVL balance(AVL T)
 {
 	if (!T)
 		return T;
-	if (DEBUG) printf("balance\n");
-	set_LC(T, balance(get_LC(T)));
-	set_RC(T, balance(get_RC(T)));
 
 	int balance_factor = get_balance(T);
-	// Single left rotation, AKA Left-Left Rotation
-	if (balance_factor > 1 && get_balance(get_LC(T)) > 0)
-	{
-		T = slr(T);
-	}
 	// Single right rotation, AKA Right-Right Rotation
-	if (balance_factor < -1 && get_balance(get_RC(T)) < 0)
+	if (balance_factor > 1 && get_balance(get_LC(T)) >= 0)
 	{
 		T = srr(T);
 	}
-	// Double left rotation, AKA Left-Right Rotation
-	if (balance_factor < 1 && get_balance(get_LC(T)) < 0)
+	// Single left rotation, AKA Left-Left Rotation
+	if (balance_factor < -1 && get_balance(get_RC(T)) <= 0)
 	{
-		T = dlr(T);
+		T = slr(T);
 	}
 	// Double right rotation, AKA Right-Left Rotation
-	if (balance_factor > -1 && get_balance(get_RC(T)) > 0)
+	if (balance_factor > 1 && get_balance(get_LC(T)) < 0)
 	{
 		T = drr(T);
+	}
+	// Double left rotation, AKA Left-Right Rotation
+	if (balance_factor < -1 && get_balance(get_RC(T)) > 0)
+	{
+		T = dlr(T);
 	}
 	return T;
 }
@@ -77,20 +74,20 @@ AVL balance(AVL T)
 //-----------------------------------------------------------------------------
 static AVL srr(AVL T)
 {
-	
 	if(DEBUG)printf("srr\n");
-	AVL new_root = get_RC(T);
-    set_RC(T, get_LC(new_root));
-    set_LC(new_root, T);
+
+	AVL new_root = get_LC(T);
+    set_LC(T, get_RC(new_root));
+    set_RC(new_root, T);
 
 	return new_root;
 }
 static AVL slr(AVL T)
 {
 	if(DEBUG)printf("slr\n");
-	AVL new_root = get_LC(T);
-    set_LC(T, get_RC(new_root));
-    set_RC(new_root, T);
+	AVL new_root = get_RC(T);
+    set_RC(T, get_LC(new_root));
+    set_LC(new_root, T);
 
 	return new_root;
 }
@@ -98,6 +95,7 @@ static AVL slr(AVL T)
 static AVL drr(AVL T)
 {
 	if(DEBUG)printf("drr\n");
+
 	AVL leftChild = get_LC(T);
     set_LC(T, slr(leftChild));
 
@@ -108,6 +106,7 @@ static AVL drr(AVL T)
 static AVL dlr(AVL T)
 {
 	if(DEBUG)printf("dlr\n");
+
 	AVL rightChild = get_RC(T);
     set_RC(T, srr(rightChild));
 
